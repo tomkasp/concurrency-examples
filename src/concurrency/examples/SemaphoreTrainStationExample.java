1ff4e6c2020d55733ad0fb2c29e58726b8eb017a
tomkasp@gmail.com <tomkasp@gmail.com>
2015-12-20 15:17:58 +0100

    Semaphore

--- src/concurrency/examples/SemaphoreTrainStationExample.java
+++ src/concurrency/examples/SemaphoreTrainStationExample.java
@@ -5,11 +5,15 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Semaphore;
 
 /**
- * Consider SemaphoreTrainStationExample as a shared resource between threads. It's like an really live. One train
+ * Consider SemaphoreTrainStationExample as a shared resource between threads. It's like an real live - One train
  * station is shared/managed by many traffic controllers.
- * 
+ *
  * Semaphore simple example based on a train station. Semaphore acts like a station witch allows to park and release
  * trains.
+ *
+ * Permit for a semaphore is like a internal counter. When the counter is grater than zero thread can acquire the
+ * resource (in our case it's SemaphoreTrainStationExample), otherwise thread goes to sleep and waits until counter
+ * is grater than zero.
  *
  * Semaphore based methods:
  * acquire() - attempt to acquire a permit (permit is a value which is set when semaphore is constructed)
