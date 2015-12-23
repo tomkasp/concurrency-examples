b390ec58ab4f264ffdc3b84e035817c06c6ba0e6
tomkasp@gmail.com <tomkasp@gmail.com>
2015-12-23 16:18:11 +0100

    reentrant lock full example with condition javadoc added

--- src/concurrency/examples/lockfullexample/ControllerTakeDownJob.java
+++ src/concurrency/examples/lockfullexample/ControllerTakeDownJob.java
@@ -1,8 +1,9 @@
 package concurrency.examples.lockfullexample;
 
 /**
- *
- * 
+ * This job is a Runnable type and it represents job done by flights controllers during take down of plains.
+ *  Run method (template hook method) runs takeDown() method which acquires a lock on ReentrantLockAirportFullExample instance.
+ *  and it case when there are no free slots for plain it puts current thread into a wait state.
  */
 public class ControllerTakeDownJob implements Runnable {
 
